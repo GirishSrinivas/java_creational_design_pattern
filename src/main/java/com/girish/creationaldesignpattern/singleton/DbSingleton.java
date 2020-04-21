2f56fd124cf97a1cfa3d2bb469bed6fcf350b8c4
GirishSrinivas <girish.s.srinivas@gmail.com>
2020-04-21 17:43:19 -0500

    Eagerly loaded singleton instance demo

---
+++ src/main/java/com/girish/creationaldesignpattern/singleton/DbSingleton.java
@@ -0,0 +1,11 @@
+package com.girish.creationaldesignpattern.singleton;
+
+public class DbSingleton {
+    private static DbSingleton instance = new DbSingleton();
+
+    private DbSingleton() {}
+
+    public static DbSingleton getInstance(){
+        return instance;
+    }
+}
---
+++ src/main/java/com/girish/creationaldesignpattern/singleton/SingletonDemoApp.java
@@ -0,0 +1,11 @@
+package com.girish.creationaldesignpattern.singleton;
+
+public class SingletonDemoApp {
+    public static void main(String[] args) {
+        DbSingleton instance = DbSingleton.getInstance();
+        System.out.println(instance);
+
+        DbSingleton anotherInstance = DbSingleton.getInstance();
+        System.out.println(anotherInstance);
+    }
+}
