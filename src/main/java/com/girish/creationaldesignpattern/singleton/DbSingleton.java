b45b810078f0139a40038c272401b383c6036370
GirishSrinivas <girish.s.srinivas@gmail.com>
2020-04-21 17:46:21 -0500

    lazy loading singleton instance demo

--- src/main/java/com/girish/creationaldesignpattern/singleton/DbSingleton.java
+++ src/main/java/com/girish/creationaldesignpattern/singleton/DbSingleton.java
@@ -1,11 +1,14 @@
 package com.girish.creationaldesignpattern.singleton;
 
 public class DbSingleton {
-    private static DbSingleton instance = new DbSingleton();
+    private static DbSingleton instance = null;
 
     private DbSingleton() {}
 
     public static DbSingleton getInstance(){
+        if(instance == null){
+            instance = new DbSingleton();
+        }
         return instance;
     }
 }
