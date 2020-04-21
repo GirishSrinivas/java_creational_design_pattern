5e4970256c5a52f9451948683d27bd41e102903d
GirishSrinivas <girish.s.srinivas@gmail.com>
2020-04-21 17:28:38 -0500

    initial commit

---
+++ src/main/java/com/girish/creationaldesignpattern/CreationalDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.girish.creationaldesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreationalDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreationalDesignPatternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/girish/creationaldesignpattern/CreationalDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.girish.creationaldesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreationalDesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
